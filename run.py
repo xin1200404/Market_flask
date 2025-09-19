fb1d34ed6d1c3369850c1da1bb3d22a50e36ded7
xin1200404 <147111028+xin1200404@users.noreply.github.com>
2025-09-18 17:14:01 -0700

    Update run.py for Render deployment

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
+import os
 from market import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #when just run in vscode, not in Render
+    #app.run(debug=True)
+
+    #to run in Render
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
 
 
 
